a687b988dffb31523236a43ad74533154053a6e2
arik-dig <asher@digma.ai>
2022-10-20 15:09:50 +0300

    remove dead code + some docs

--- instrumentation/spring/spring-boot-autoconfigure/src/main/java/com/digma/otel/instrumentation/spring/autoconfigure/resources/DigmaResourceAutoConfiguration.java
+++ instrumentation/spring/spring-boot-autoconfigure/src/main/java/com/digma/otel/instrumentation/spring/autoconfigure/resources/DigmaResourceAutoConfiguration.java
@@ -1,30 +1,22 @@
 package com.digma.otel.instrumentation.spring.autoconfigure.resources;
 
-import io.opentelemetry.api.common.AttributeKey;
-import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.instrumentation.spring.autoconfigure.OpenTelemetryAutoConfiguration;
-import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 import io.opentelemetry.sdk.autoconfigure.spi.ResourceProvider;
-import io.opentelemetry.sdk.resources.Resource;
-import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static io.opentelemetry.api.common.AttributeKey.stringKey;
-
+/**
+ * DigmaResourceAutoConfiguration.
+ * looks similar to {@link io.opentelemetry.instrumentation.spring.autoconfigure.resources.OtelResourceAutoConfiguration}.
+ */
 @Configuration
 @EnableConfigurationProperties(DigmaResourceProperties.class)
 @AutoConfigureBefore(OpenTelemetryAutoConfiguration.class)
 @ConditionalOnProperty(prefix = "digma.otel.springboot.resource", name = "enabled", matchIfMissing = true)
 public class DigmaResourceAutoConfiguration {
-
-    @Bean
-    public ResourceProvider dummyResourceProvider() {
-        return new DumResourceProvider();
-    }
 
     /**
      * this bean will get merged with {@link OpenTelemetryAutoConfiguration.OpenTelemetryBeanConfig#otelResource}
@@ -32,20 +24,6 @@ public class DigmaResourceAutoConfiguration {
     @Bean
     public ResourceProvider digmaResourceProvider(DigmaResourceProperties properties) {
         return new DigmaResourceProvider(properties);
-    }
-
-    private static final AttributeKey<String> DUMMY_ATTR = stringKey("dummy.attr");
-
-    static class DumResourceProvider implements ResourceProvider {
-
-        @Override
-        public Resource createResource(ConfigProperties config) {
-            Attributes attrs = Attributes.of(
-                    DUMMY_ATTR, "dummy.val");
-
-            return Resource.create(attrs,
-                    ResourceAttributes.SCHEMA_URL);
-        }
     }
 
 }
--- instrumentation/spring/spring-boot-autoconfigure/src/main/java/com/digma/otel/instrumentation/spring/autoconfigure/resources/DigmaResourceProperties.java
+++ instrumentation/spring/spring-boot-autoconfigure/src/main/java/com/digma/otel/instrumentation/spring/autoconfigure/resources/DigmaResourceProperties.java
@@ -5,6 +5,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "digma.otel.springboot.resource")
 public class DigmaResourceProperties {
     private boolean enabled = true;
+    /**
+     * environment.
+     * environment which the application runs at. possible values: PRODUCTION, QA, CI.
+     * you can leave it empty and Digma will evaluate it in the following order:
+     * 1. read environment variable named DEPLOYMENT_ENV
+     * 2. get local hostname, and append const values [local]. for example: for local machine named myhost it will return myhost[local]
+     */
     private String environment;
 
     public boolean isEnabled() {
