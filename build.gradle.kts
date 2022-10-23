37dd01ddf1fb9c4d9ddd6a194fd2de0a86005694
arik-dig <asher@digma.ai>
2022-10-23 10:04:20 +0300

    enforce java version 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,18 @@
 plugins {
-  id("java")
-  id("idea")
-  id("maven-publish")
+    id("java")
+    id("idea")
+    id("maven-publish")
 }
 
 description = "Digma OpenTelemetry instrumentations for Java"
 group = "com.digma"
 
 allprojects {
-  version = "0.0.8"
+    version = "0.0.9"
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    javaToolchains.launcherFor {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
 }
