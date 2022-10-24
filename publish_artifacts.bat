0f11b752d498f5a2b13e4e019a8879c7b25c8873
arik-dig <asher@digma.ai>
2022-10-24 16:53:54 +0300

    Feature/Gradle publish - parallel

--- publish_artifacts.bat
+++ publish_artifacts.bat
@@ -9,7 +9,7 @@ rem ----------------------------------------------------------------------------
 
 set scriptDir=%~dp0
 
-gradlew publish ^
+gradlew publish --parallel ^
  -Psigning.secretKeyRingFile=%scriptDir%/xtra/security/secr-key-ring-file-gpg.bin ^
  -Psigning.password=%EV_signing_password% ^
  -Psigning.keyId=%EV_signing_keyId%
