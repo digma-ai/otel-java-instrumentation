d9008b037359f60ddea8dfa2dcea4d16286f687f
arik-dig <asher@digma.ai>
2022-10-24 14:21:57 +0300

    pgp file, and usage of it

---
+++ publish_artifacts.bat
@@ -0,0 +1,15 @@
+@echo off
+rem ----------------------------------------------------------------------------
+rem - prior to running this script need to set the following Environment Variables :
+rem -  EV_signing_keyId
+rem -  EV_signing_password
+rem -  EV_ossrhUsername
+rem -  EV_ossrhPassword
+rem ----------------------------------------------------------------------------
+
+set scriptDir=%~dp0
+
+gradlew publish ^
+ -Psigning.secretKeyRingFile=%scriptDir%/xtra/security/secr-key-ring-file-gpg.bin ^
+ -Psigning.password=%EV_signing_password% ^
+ -Psigning.keyId=%EV_signing_keyId%
---
+++ xtra/security/README.md
@@ -0,0 +1,8 @@
+##
+
+### file secr-key-ring-file-gpg
+
+following the guide of [OSSRH](https://central.sonatype.org/publish/requirements/gpg/)
+we had to generate key using GPG software.
+then we followed guide of [Gradle Signing Plugin](https://docs.gradle.org/current/userguide/signing_plugin.html)
+and we exported our key to this pgp file and kept it locally.
