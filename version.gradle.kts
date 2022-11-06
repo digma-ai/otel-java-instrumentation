2e0ad384e7ccf6b94d7e09b012b7d0fa5f3eabdb
arik-dig <asher@digma.ai>
2022-11-06 17:59:58 +0200

    move the version to dedicated file

---
+++ version.gradle.kts
@@ -0,0 +1,7 @@
+// keep the name theVersion and the val definition since script update_version.sh maintains it
+val theVersion = "0.2.2"
+
+allprojects {
+    group = "io.github.digma-ai"
+    version = theVersion
+}
