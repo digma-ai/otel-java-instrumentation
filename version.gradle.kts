68a57be2df87946edd25d2f8ec8f85c646477c12
arik-dig <asher@digma.ai>
2022-11-07 16:18:25 +0200

    take 3

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,5 @@
 // keep the name theVersion and the val definition since script update_version.sh maintains it
-val theVersion = "0.2.2"
+val theVersion = "0.2.17"
 
 allprojects {
     group = "io.github.digma-ai"
