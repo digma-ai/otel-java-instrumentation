08b3cb85edd99abe8fb462175808506c35ef0e56
arik-dig <asher@digma.ai>
2022-11-15 15:40:52 +0200

    remove grpc javaagent

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -38,7 +38,6 @@ rootProject.name = "digma-otel-java-instrumentation"
 //include(":instrumentation:spring:spring-webmvc-5.3:library")
 include(":instrumentation:common")
 include(":instrumentation:spring:spring-boot-autoconfigure")
-include(":instrumentation:grpc-16:javaagent")
 include(":instrumentation:grpc-16:library")
 //include(":instrumentation:spring:starters:spring-boot-starter")
 
