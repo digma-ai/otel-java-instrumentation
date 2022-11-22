776444fba12b39d66862aa8fa8497f868524c27a
arik-dig <asher@digma.ai>
2022-11-22 12:27:35 +0200

    DigmaAutoResourceProvider - use logging

--- agent-extension/src/main/java/com/digma/otel/javaagent/extension/DigmaAutoResourceProvider.java
+++ agent-extension/src/main/java/com/digma/otel/javaagent/extension/DigmaAutoResourceProvider.java
@@ -10,8 +10,20 @@ import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 import io.opentelemetry.sdk.autoconfigure.spi.ResourceProvider;
 import io.opentelemetry.sdk.resources.Resource;
 
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.FINE;
+
 @AutoService(ResourceProvider.class)
 public class DigmaAutoResourceProvider implements ResourceProvider {
+
+    private static final Logger logger = Logger.getLogger(DigmaAutoResourceProvider.class.getName());
+
+    public DigmaAutoResourceProvider() {
+        if (logger.isLoggable(FINE)) {
+            logger.log(FINE, "DigmaAutoResourceProvider been constructed");
+        }
+    }
 
     @Override
     public Resource createResource(ConfigProperties config) {
@@ -26,6 +38,9 @@ public class DigmaAutoResourceProvider implements ResourceProvider {
         }
 
         Attributes attributes = attributesBuilder.build();
+        if (logger.isLoggable(FINE)) {
+            logger.log(FINE, "DigmaAutoResourceProvider created resources with attributes: {0}", attributes);
+        }
         return Resource.create(attributes);
     }
 }
