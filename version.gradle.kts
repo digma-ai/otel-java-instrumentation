decaf369efda31a3d8d836df4e53c8ee88977d02
arik-dig <asher@digma.ai>
2022-11-24 16:56:06 +0200

    add jar attributes

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,5 @@
 // keep the name theVersion and the val definition since script update_version.sh maintains it
-val theVersion = "0.4.15-SNAPSHOT"
+val theVersion = "0.4.16-SNAPSHOT"
 
 allprojects {
     group = "io.github.digma-ai"
