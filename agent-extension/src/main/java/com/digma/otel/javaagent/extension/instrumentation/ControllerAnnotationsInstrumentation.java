7b6f3fc820cdf212a128f1c112fe20208507ae4c
arik-dig <asher@digma.ai>
2022-11-24 08:57:56 +0200

    using DigmaServerAdvice

--- agent-extension/src/main/java/com/digma/otel/javaagent/extension/instrumentation/ControllerAnnotationsInstrumentation.java
+++ agent-extension/src/main/java/com/digma/otel/javaagent/extension/instrumentation/ControllerAnnotationsInstrumentation.java
@@ -1,15 +1,10 @@
 package com.digma.otel.javaagent.extension.instrumentation;
 
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.instrumentation.api.instrumenter.LocalRootSpan;
+import com.digma.otel.javaagent.extension.instrumentation.common.DigmaServerAdvice;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
-import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
-import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
-
-import java.lang.reflect.Method;
 
 import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
 import static net.bytebuddy.matcher.ElementMatchers.inheritsAnnotation;
@@ -45,32 +40,6 @@ public class ControllerAnnotationsInstrumentation implements TypeInstrumentation
                     "org.springframework.web.bind.annotation.PutMapping",
                     "org.springframework.web.bind.annotation.PatchMapping"
                 ))),
-            ControllerAnnotationsInstrumentation.class.getName() + "$ControllerAnnotationsAdvice");
-    }
-
-    @SuppressWarnings("unused")
-    public static class ControllerAnnotationsAdvice {
-
-        @Advice.OnMethodEnter(
-            // suppress = Throwable.class
-        )
-        public static void methodEnter(
-            @Advice.This Object target,
-            @Advice.Origin Method method,
-            @Advice.Origin String methodFqn) {
-
-            Class<?> classOfTarget = target.getClass();
-            // taking local root span (servlet of tomcat or jetty) and set the code attributes on it
-            Span localRootSpan = LocalRootSpan.current();
-
-            localRootSpan.setAttribute(SemanticAttributes.CODE_NAMESPACE, classOfTarget.getName());
-            localRootSpan.setAttribute(SemanticAttributes.CODE_FUNCTION, method.getName());
-        }
-
-        @Advice.OnMethodExit(
-            // suppress = Throwable.class
-        )
-        public static void methodExit() {
-        }
+            DigmaServerAdvice.class.getName());
     }
 }
