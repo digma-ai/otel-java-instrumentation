6607eafa7a1e30c162cc787eae6561d8f4b115fb
arik-dig <asher@digma.ai>
2022-11-27 14:56:50 +0200

    Feature/Add source and javadoc + remove the "-all"

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,5 @@
 // keep the name theVersion and the val definition since script update_version.sh maintains it
-val theVersion = "0.4.19-SNAPSHOT"
+val theVersion = "0.4.20-SNAPSHOT"
 
 allprojects {
     group = "io.github.digma-ai"
