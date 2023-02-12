0c42733249a2ac3c35cd79517978c61b8b489001
arik-dig <asher@digma.ai>
2023-02-12 15:54:53 +0200

    referring generated class DigmaExtensionVersion

--- agent-extension/src/main/java/com/digma/otel/javaagent/extension/AgentExtensionVersion.java
+++ agent-extension/src/main/java/com/digma/otel/javaagent/extension/AgentExtensionVersion.java
@@ -1,9 +1,12 @@
 package com.digma.otel.javaagent.extension;
 
+import com.digma.otel.javaagent.extension.version.DigmaExtensionVersion;
+
 public final class AgentExtensionVersion {
 
-    public static final String VERSION =
-        AgentExtensionVersion.class.getPackage().getImplementationVersion();
+    public static final String VERSION = DigmaExtensionVersion.VERSION;
+    // getPackage().getImplementationVersion() doesn't work via OTEL agent extension
+    //AgentExtensionVersion.class.getPackage().getImplementationVersion();
 
     private AgentExtensionVersion() {
     }
