e99855d3f7be7c4d7eb27e44d0430a157e14e003
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2023-07-13 17:59:58 +0300

    version 0.7.2

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,5 @@
 // keep the name theVersion and the val definition since script update_version.sh maintains it
-val theVersion = "0.7.1.101-SNAPSHOT"
+val theVersion = "0.7.2"
 
 allprojects {
     group = "io.github.digma-ai"
