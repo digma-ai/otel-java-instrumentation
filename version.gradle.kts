95a5a5bec86d41852241e060902b9ac5576aa9be
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2023-12-14 17:59:41 +0200

    version snapshot

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,5 @@
 // keep the name theVersion and the val definition since script update_version.sh maintains it
-val theVersion = "0.8.3-SNAPSHOT"
+val theVersion = "0.8.29-SNAPSHOT"
 
 allprojects {
     group = "io.github.digma-ai"
