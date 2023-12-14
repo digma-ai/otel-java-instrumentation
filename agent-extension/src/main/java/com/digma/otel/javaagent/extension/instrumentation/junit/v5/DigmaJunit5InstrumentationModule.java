d58c7307eebe3c474fd38d3e852cc912aa9a16a3
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2023-12-14 14:34:08 +0200

    start junit instrumentation

---
+++ agent-extension/src/main/java/com/digma/otel/javaagent/extension/instrumentation/junit/v5/DigmaJunit5InstrumentationModule.java
@@ -0,0 +1,35 @@
+package com.digma.otel.javaagent.extension.instrumentation.junit.v5;
+
+import com.google.auto.service.AutoService;
+import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import net.bytebuddy.matcher.ElementMatcher;
+
+import java.util.Collections;
+import java.util.List;
+
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
+
+@AutoService(InstrumentationModule.class)
+public class DigmaJunit5InstrumentationModule extends InstrumentationModule {
+
+    public DigmaJunit5InstrumentationModule() {
+        super("digma-junit-5.0");
+    }
+
+    @Override
+    public int order() {
+        return 111; // should be triggered after original instrumentations
+    }
+
+    // require junit 5
+    @Override
+    public ElementMatcher.Junction<ClassLoader> classLoaderMatcher() {
+        return hasClassesNamed("org.junit.jupiter.api.Test");
+    }
+
+    @Override
+    public List<TypeInstrumentation> typeInstrumentations() {
+        return Collections.singletonList(new Junit5AnnotationsInstrumentation());
+    }
+}
---
+++ agent-extension/src/main/java/com/digma/otel/javaagent/extension/instrumentation/junit/v5/Junit5AnnotationsInstrumentation.java
@@ -0,0 +1,38 @@
+package com.digma.otel.javaagent.extension.instrumentation.junit.v5;
+
+import com.digma.otel.javaagent.extension.instrumentation.common.DigmaServerAdvice;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
+import static net.bytebuddy.matcher.ElementMatchers.declaresMethod;
+import static net.bytebuddy.matcher.ElementMatchers.isAnnotatedWith;
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.namedOneOf;
+
+public class Junit5AnnotationsInstrumentation implements TypeInstrumentation {
+
+    @Override
+    public ElementMatcher<ClassLoader> classLoaderOptimization() {
+        return hasClassesNamed("org.junit.jupiter.api.Test");
+    }
+
+    @Override
+    public ElementMatcher<TypeDescription> typeMatcher() {
+        return declaresMethod(isAnnotatedWith(named("org.junit.jupiter.api.Test")));
+    }
+
+    @Override
+    public void transform(TypeTransformer transformer) {
+        transformer.applyAdviceToMethod(
+                isMethod()
+                        .and(
+                                isAnnotatedWith(namedOneOf(
+                                        "org.junit.jupiter.api.Test"
+                                ))),
+                DigmaServerAdvice.class.getName());
+    }
+}
