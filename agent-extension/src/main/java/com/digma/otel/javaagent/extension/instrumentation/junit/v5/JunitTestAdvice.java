6276331e61f9ac462d887ac6741b0aa2ef3149c0
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2023-12-14 17:56:56 +0200

    advice works!

--- agent-extension/src/main/java/com/digma/otel/javaagent/extension/instrumentation/junit/v5/JunitTestAdvice.java
+++ agent-extension/src/main/java/com/digma/otel/javaagent/extension/instrumentation/junit/v5/JunitTestAdvice.java
@@ -10,22 +10,26 @@ import java.lang.reflect.Method;
 
 import static com.digma.otel.javaagent.extension.instrumentation.junit.v5.JunitSingletons.instrumenter;
 
-
+/**
+ * took idea from OTEL implementation of WithSpan.
+ *
+ * see https://github.com/open-telemetry/opentelemetry-java-instrumentation/blob/release/v1.32.x/instrumentation/opentelemetry-extension-annotations-1.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/extensionannotations/WithSpanInstrumentation.java
+ */
 public class JunitTestAdvice {
 
     @Advice.OnMethodEnter(suppress = Throwable.class)
-    public static void onEnter(
+    public static void methodEnter(
             @Advice.Origin String methodFqn,
             @Advice.Origin Method originMethod,
             @Advice.Local("otelMethod") Method method,
             @Advice.Local("otelContext") Context context,
             @Advice.Local("otelScope") Scope scope) {
 
-        System.out.println("DBG: JunitTestAdvice.OnMethodEnter methodFqn=" + methodFqn);
-
         // Every usage of @Advice.Origin Method is replaced with a call to Class.getMethod, copy it
         // to local variable so that there would be only one call to Class.getMethod.
         method = originMethod;
+
+//        System.out.println("DBG: JunitTestAdvice.OnMethodEnter methodFqn=" + methodFqn);
 
         Instrumenter<Method, Object> instrumenter = instrumenter();
         Context current = Java8BytecodeBridge.currentContext();
@@ -37,19 +41,18 @@ public class JunitTestAdvice {
     }
 
     @Advice.OnMethodExit(onThrowable = Throwable.class, suppress = Throwable.class)
-    public static void stopSpan(
+    public static void methodExit(
             @Advice.Local("otelMethod") Method method,
             @Advice.Local("otelContext") Context context,
             @Advice.Local("otelScope") Scope scope,
             @Advice.Thrown Throwable throwable) {
+
         if (scope == null) {
             return;
         }
         scope.close();
 
-        if (throwable != null) {
-            instrumenter().end(context, method, null, throwable);
-        }
+        instrumenter().end(context, method, null, throwable);
     }
 
 }
