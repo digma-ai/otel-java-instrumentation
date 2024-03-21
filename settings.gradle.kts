889872ea71316497e9a59a6e87e5fba9e5da5dcc
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-03-21 02:30:47 +0200

    add jdk repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,4 @@
 pluginManagement {
-  plugins {
-    id("com.bmuschko.docker-remote-api") version "7.3.0"
-    id("com.github.jk1.dependency-license-report") version "2.1"
-    id("com.google.cloud.tools.jib") version "3.2.1"
-    id("com.gradle.plugin-publish") version "1.0.0"
-    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-    id("org.jetbrains.kotlin.jvm") version "1.6.20"
-    id("org.unbroken-dome.test-sets") version "4.0.0"
-    id("org.xbib.gradle.plugin.jflex") version "1.6.0"
-    id("org.unbroken-dome.xjc") version "2.0.0"
-  }
-
   repositories {
     mavenCentral()
     gradlePluginPortal()
@@ -18,7 +6,7 @@ pluginManagement {
 }
 
 plugins {
-  id("com.github.burrunan.s3-build-cache") version "1.3"
+  id("org.gradle.toolchains.foojay-resolver-convention") version("0.7.0")
 }
 
 dependencyResolutionManagement {
