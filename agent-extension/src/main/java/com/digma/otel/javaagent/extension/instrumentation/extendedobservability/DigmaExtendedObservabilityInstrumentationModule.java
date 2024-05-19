2af8b0d01b3836545daa6338bbed771c6d9144ea
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-05-19 17:18:52 +0300

    support span attributes for extended observability Closes #69 (#70)

---
+++ agent-extension/src/main/java/com/digma/otel/javaagent/extension/instrumentation/extendedobservability/DigmaExtendedObservabilityInstrumentationModule.java
@@ -0,0 +1,39 @@
+package com.digma.otel.javaagent.extension.instrumentation.extendedobservability;
+
+import com.digma.otel.javaagent.extension.version.DigmaExtensionVersion;
+import com.google.auto.service.AutoService;
+import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import net.bytebuddy.matcher.ElementMatcher;
+
+import java.util.Collections;
+import java.util.List;
+
+import static io.opentelemetry.javaagent.extension.matcher.AgentElementMatchers.hasClassesNamed;
+
+
+@AutoService(InstrumentationModule.class)
+public class DigmaExtendedObservabilityInstrumentationModule extends InstrumentationModule {
+
+    public static final String DIGMA_MARKER_ANNOTATION_NAME = "org.digma.instrumentation.ExtendedObservability";
+
+    public DigmaExtendedObservabilityInstrumentationModule() {
+        super("digma-extended","digma-extended-"+ DigmaExtensionVersion.VERSION);
+    }
+
+    @Override
+    public int order() {
+        return 10000;
+    }
+
+    @Override
+    public ElementMatcher.Junction<ClassLoader> classLoaderMatcher() {
+        return hasClassesNamed(DIGMA_MARKER_ANNOTATION_NAME);
+    }
+
+    @Override
+    public List<TypeInstrumentation> typeInstrumentations() {
+        return Collections.singletonList(new ExtendedObservabilityTypeInstrumentation());
+    }
+
+}
